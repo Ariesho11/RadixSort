88445d1ec18be80e7b78cfb546bb351438f45418
Aries <aho11@stuy.edu>
2021-01-13 09:07:50 -0500

    nth method kind of works

---
+++ Radix.java
@@ -0,0 +1,31 @@
+public class Radix{
+  public static int nth(int n, int col){
+    int num = (int)Math.pow(10, col);
+    if (col == 0) num = 10;
+    return n%num;
+  }
+  // get nth digit of an int, where 0 is the ones column, 1 is the tens column etc.
+  // nth(123,1) -> 2
+  // nth(-123,1) -> 2
+  // nth(123,2) -> 1
+  // nth(-123,2) -> 1
+
+  public static int length(int n){
+    return 1;
+  }
+  // return the number of digits in n.
+  // length(0) -> 1
+  // length(15) -> 2
+  // length(-10) -> 2
+  // length(5112) -> 4
+
+  //public static void merge(MyLinkedList original,MyLinkedList[]buckets){
+
+//  }
+  // Merge all of the linked lists in the bucket array into your original
+  // linked list. The original may have elements, and those should be kept.
+  // This is O(buckets.length) which should be 10 when we use this later.
+  // The bucket[0] list will be merged first, then the bucket[1] etc.
+  // Write and thoroughly test these methods!
+
+}
